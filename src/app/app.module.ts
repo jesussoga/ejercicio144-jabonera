a1084756a9709fe667cdfec1e462e6b110bc4598
“jesus” <em.sotoca@gmail.com>
2023-06-12 09:01:16 +0200

    modificación en el app.modules y app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JaboneraComponent } from './componets/jabonera/jabonera.component';
+import { ManoComponent } from './componets/mano/mano.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JaboneraComponent,
+    ManoComponent
   ],
   imports: [
     BrowserModule
